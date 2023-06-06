f4602beb51b39db18edd70bd04bee8eef6be2ec1
Stanislav <anoshin.stanislav@yandex.ru>
2023-06-06 13:35:07 +0500

    HW9.1

---
+++ HW9.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter N: ");
+int num1 = 1;
+int num2 = int.Parse(Console.ReadLine());
+
+Numbers(num1, num2);
+
+int Numbers(int number1, int number2)
+{
+    if(number1 > number2) return number2;
+    
+    else
+    {
+        Console.Write(number1 + ", ");
+        number1++;
+    }
+    
+    return Numbers(number1++, number2);
+}
+
