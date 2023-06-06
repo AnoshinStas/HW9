7a2e5999368d5c645f556afa4d3a15dd8b65b9dc
Stanislav <anoshin.stanislav@yandex.ru>
2023-06-06 14:05:13 +0500

    HW9.3

---
+++ HW9.3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = int.Parse(Console.ReadLine());
+ 
+Console.WriteLine($"{Pow(m,n)}");
+ 
+int Pow(int number,int degree)
+{
+   if (degree == 0) return 1;
+   if(degree == 1) return number;
+   return (number * Pow(number, degree-1));
+}
